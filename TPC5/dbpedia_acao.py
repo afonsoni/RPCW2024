c53bcfc3c0de1d1fa5f79efca97488f4325be34f
Afonso Ferreira <29693141+afonsoni@users.noreply.github.com>
2024-03-19 08:30:27 +0000

    short_film_and_action_film

--- TPC5/dbpedia_acao.py
+++ TPC5/dbpedia_acao.py
@@ -9,14 +9,34 @@ sparql_query = """
 PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-SELECT DISTINCT ?filme ?label ?abstract
+SELECT DISTINCT ?filme ?label
+        (GROUP_CONCAT(DISTINCT ?directorLabel; SEPARATOR=", ") AS ?directorLabels)
+        (GROUP_CONCAT(DISTINCT ?starringLabel; SEPARATOR=", ") AS ?starringLabels)
+        (GROUP_CONCAT(DISTINCT ?writerLabel; SEPARATOR=", ") AS ?writerLabels)
+        (GROUP_CONCAT(DISTINCT ?musicComposerLabel; SEPARATOR=", ") AS ?musicComposerLabels)
+        (GROUP_CONCAT(DISTINCT ?cinematographyLabel; SEPARATOR=", ") AS ?cinematographyLabels)
 WHERE {
     ?filme a dbo:Film ;
         rdfs:label ?label ;
-        dbo:abstract ?abstract ;
         dbo:wikiPageWikiLink dbr:Action_film .
-    FILTER (LANG(?label) = 'en' && LANG(?abstract) = 'en') .
+OPTIONAL { ?filme dbo:director ?director .
+            ?director rdfs:label ?directorLabel .
+            FILTER (LANG(?directorLabel) = 'en') }
+OPTIONAL { ?filme dbo:starring ?starring .
+            ?starring rdfs:label ?starringLabel .
+            FILTER (LANG(?starringLabel) = 'en') }
+OPTIONAL { ?filme dbo:writer ?writer .
+            ?writer rdfs:label ?writerLabel .
+            FILTER (LANG(?writerLabel) = 'en') }
+OPTIONAL { ?filme dbo:musicComposer ?musicComposer .
+            ?musicComposer rdfs:label ?musicComposerLabel .
+            FILTER (LANG(?musicComposerLabel) = 'en') }
+OPTIONAL { ?filme dbo:cinematography ?cinematography .
+            ?cinematography rdfs:label ?cinematographyLabel .
+            FILTER (LANG(?cinematographyLabel) = 'en') }
+    FILTER (LANG(?label) = 'en') .
 }
+GROUP BY ?filme ?label
 ORDER BY ?label
 """
 
@@ -43,7 +63,11 @@ if response.status_code == 200:
         filmes_acao.append({
             "uri": result["filme"]["value"],
             "designacao": result["label"]["value"],
-            "descricao": result["abstract"]["value"]
+            "ator(es)": result["starringLabels"]["value"],
+            "realizador": result["directorLabels"]["value"],
+            "escritor": result["writerLabels"]["value"],
+            "musico": result["musicComposerLabels"]["value"],
+            "cinematografia": result["cinematographyLabels"]["value"]
         })
     f = open("filmes_acao.json", "w")
     json.dump(filmes_acao, f)
